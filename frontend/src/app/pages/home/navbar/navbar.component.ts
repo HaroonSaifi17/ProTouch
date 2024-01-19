9ba35f35738d9f8e8fdaff2ca91f1a332d19f696
Mohd Haroon <haroonsaifi2001@gmail.com>
2024-01-19 20:31:20 +0530

    initial commit

---
+++ frontend/src/app/pages/home/navbar/navbar.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent implements OnInit {
+  constructor(private router: Router) { }
+  ngOnInit(): void { }
+  public navStatus: boolean = false
+  toggleClass(list: HTMLDivElement): void {
+    if(window.innerWidth>=640){
+      return
+    }
+    if (list.style.right == '0px') {
+      list.style.right = '-100%'
+      this.navStatus = false
+      document.body.classList.remove('scroll-lock')
+    } else {
+      list.style.right = '0px'
+      this.navStatus = true
+      document.body.classList.add('scroll-lock')
+    }
+  }
+  adminLogin() {
+    this.router.navigateByUrl('adminlogin')
+  }
+  navigate(fregment: string): void {
+    this.router.navigateByUrl('' + fregment)
+  }
+}
